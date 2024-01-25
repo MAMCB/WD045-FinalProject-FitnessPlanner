9b3e6e9cb7e4f9a5105aaec896bb83afc04fe85f
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-01-25 15:36:45 +0100

    created axios instance, set up frontend routes and set up components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import Home from './components/Home'
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
+import WorkoutPlan from '../../server/models/workoutPlan'
+import Exercise from './components/Exercise'
+import WorkoutPlayer from './components/WorkoutPlayer'
+import Editor from './components/Editor'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
+import LandingPage from './components/LandingPage'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +18,21 @@ function App() {
   return (
     <>
     <Routes>
-
       <Route path="/" element={<Home />} />
+      <Route path='/landingPage' element={<LandingPage />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/exercise' element={<Exercise />} />
+      <Route path='/exercise/:id' element={<Exercise />} />
+      <Route path='/workoutPlan' element={<WorkoutPlan/>}/>
+      <Route path='/workoutPlan/public' element={<WorkoutPlan/>}/>
+      <Route path='/workoutPlan/public/:id' element={<WorkoutPlan/>}/>
+      <Route path='/workoutPlan/:id' element={<WorkoutPlan/>}/>
+      <Route path='/editor' element={<Editor/>}/>
+      <Route path='/workoutPlayer' element={<WorkoutPlayer/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+          
+      
     </Routes>
       
     </>
---
+++ client/src/components/WorkoutPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkoutPlayer = () => {
+  return (
+    <div>WorkoutPlayer</div>
+  )
+}
+
+export default WorkoutPlayer

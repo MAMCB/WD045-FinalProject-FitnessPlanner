2c4e334381e02fd1fd9bb90ce3cfba2482fa19fb
AnnaShazk <dream1nd.find@gmail.com>
2024-01-25 17:35:09 +0100

    fix some problem in form

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,8 +10,9 @@ export default {
     extend: {
       colors: {
         background: "#F8FCFB",
-        primary: "#4FB5A9",
+        primaryOne: "#4FB5A9",
         secondary: "##9eb9d6",
+        primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a","950":"#172554"},
         accent: "##7389c4",
       },
     },

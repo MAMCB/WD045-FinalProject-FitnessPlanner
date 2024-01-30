fda1c42692bfb75c703782e4de068ab2b45b0173
MAMCB <96302464+MAMCB@users.noreply.github.com>
2024-01-30 10:18:21 +0100

    typo fix for navbar import

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,7 +5,7 @@ import { AuthContext } from "../context/Auth";
 import { Navigate } from "react-router-dom";
 import { useForm, FormProvider, useFormContext } from "react-hook-form";
 import { Input } from "./Input";
-import NavbarForHome from "./NavbarforHome";
+import NavbarForHome from "./NavbarForHome";
 const Login = () => {
   const context = useContext(AuthContext);
   const errors_ = context.errors;

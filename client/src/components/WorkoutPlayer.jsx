c7cacfc9f4e69d353b8c7b2f16c77ca4a582f0e0
AnnaShazk <dream1nd.find@gmail.com>
2024-01-31 13:19:30 +0100

    finished the logic in component

--- client/src/components/WorkoutPlayer.jsx
+++ client/src/components/WorkoutPlayer.jsx
@@ -3,15 +3,21 @@ import { useState, useEffect } from "react";
 import axiosInstance from "../axiosInstance";
 import StopButton from "../assets/stop-button.png";
 import PauseButton from "../assets/pause-button.png";
+import { set } from "react-hook-form";
 
 const WorkoutPlayer = () => {
   const [workoutData, setWorkoutData] = useState({});
   const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
   const [remainingTime, setRemainingTime] = useState(0);
+  const [remainingTimeInRest, setRemainingTimeInRest] = useState(0);
+  const [isWorkoutStarted, setIsWorkoutStarted] = useState(false);
+  const [isWorkoutPaused, setIsWorkoutPaused] = useState(false);
+  const [isWorkoutFinished, setIsWorkoutFinished] = useState(false);
+  const [isExerciseFinished, setIsExerciseFinished] = useState(false);
 
   const workoutPlanId = "60f9b4b3c9b9a40015f3b3b2";
 
-  useEffect(() => {
+  /*   useEffect(() => {
     axiosInstance
       .get(
         import.meta.env.VITE_SERVER_BASE_URL +
@@ -23,7 +29,7 @@ const WorkoutPlayer = () => {
         setWorkoutData(workoutData);
       })
       .catch((err) => console.log("Error:", err));
-  }, []);
+  }, []); */
 
   // example of workout data :
 
@@ -40,57 +46,83 @@ const WorkoutPlayer = () => {
     rating: 4.5,
     exercises: [
       {
-        bodyPart: "waist",
-        equipment: "body weight",
-        gifUrl: "https://v2.exercisedb.io/image/q7jU65ylp1U00N",
+        equipment: "leverage machine",
+        image: "https://v2.exercisedb.io/image/ocPU7YLtlQTGXe",
         id: "0002",
-        name: "45° side bend",
-        target: "abs",
-        secondaryMuscles: ["obliques"],
-        instructions: [
-          "Stand with your feet shoulder-width apart and your arms extended straight down by your sides.",
-          "Keeping your back straight and your core engaged, slowly bend your torso to one side, lowering your hand towards your knee.",
-          "Pause for a moment at the bottom, then slowly return to the starting position.",
-          "Repeat on the other side.",
-          "Continue alternating sides for the desired number of repetitions.",
-        ],
+        name: "lever seated hip adduction",
+        muscleGroup: "adductors",
+        description:
+          "Adjust the seat height and position yourself on the machine with your back against the backrest.Place your feet on the footrests and grasp the handles for stability.Engage your adductor muscles and slowly bring your legs together, squeezing your inner thighs.Pause for a moment at the peak contraction, then slowly return to the starting position.Repeat for the desired number of repetitions.",
       },
       {
-        bodyPart: "waist",
         equipment: "body weight",
-        gifUrl: "https://v2.exercisedb.io/image/O7Wt-wHdMcZLmf",
+        image: "https://v2.exercisedb.io/image/iF0rL7-HK5vaK5",
         id: "0001",
-        name: "3/4 sit-up",
-        target: "abs",
+        name: "butterfly yoga pose",
+        muscleGroup: "adductors",
         secondaryMuscles: ["hip flexors", "lower back"],
-        instructions: [
-          "Lie flat on your back with your knees bent and feet flat on the ground.",
-          "Place your hands behind your head with your elbows pointing outwards.",
-          "Engaging your abs, slowly lift your upper body off the ground, curling forward until your torso is at a 45-degree angle.",
-          "Pause for a moment at the top, then slowly lower your upper body back down to the starting position.",
-          "Repeat for the desired number of repetitions.",
-        ],
+        description:
+          "Sit on the floor with your legs extended in front of you.Bend your knees and bring the soles of your feet together, allowing your knees to fall out to the sides.Hold onto your ankles or feet with your hands.Sit up tall and lengthen your spine.Gently press your knees down towards the floor, feeling a stretch in your inner thighs.Hold this position for a few breaths.To release, slowly bring your knees back up and extend your legs.",
       },
     ],
-    exerciseDuration: 30,
-    restDuration: 10,
+    exerciseDuration: 10,
+    restDuration: 5,
     createdDay: new Date(),
   };
 
-useEffect(() => {
-  setRemainingTime(workoutDataExample.exerciseDuration);
-  const timerId = setInterval(() => {
-    setRemainingTime((prevTime) => {
-      if (prevTime === 1) {
-        setCurrentExerciseIndex((prevIndex) => prevIndex + 1);
-        return workoutDataExample.exerciseDuration; // Reset the timer for the next exercise
-      }
-      return prevTime - 1;
-    });
-  }, 1000);
+  useEffect(() => {
+    if (
+      isExerciseFinished &&
+      currentExerciseIndex < workoutDataExample.exercises.length
+    ) {
+      setRemainingTimeInRest(workoutDataExample.restDuration);
+      const restTimerId = setInterval(() => {
+        setRemainingTimeInRest((prevTime) => {
+          if (prevTime === 1) {
+            setIsExerciseFinished(false);
+            return () => clearInterval(restTimerId);
+          }
+          return prevTime - 1;
+        });
+      }, 1000); // 1000 ms = 1s
+      return () => clearInterval(restTimerId);
+    } else {
+      setRemainingTimeInRest(0);
+      console.log("Rest time finished");
+    }
+  }, [isExerciseFinished]);
 
-  return () => clearInterval(timerId);
-}, [currentExerciseIndex]);
+  useEffect(() => {
+    if (
+      currentExerciseIndex < workoutDataExample.exercises.length &&
+      !isExerciseFinished
+    ) {
+      setRemainingTime(workoutDataExample.exerciseDuration);
+      const timerId = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime === 1) {
+            setIsExerciseFinished(true);
+            setCurrentExerciseIndex((prevIndex) => prevIndex + 1);
+
+            return workoutDataExample.exerciseDuration; // Reset the timer for the next exercise
+          }
+          console.log(prevTime);
+          console.log(currentExerciseIndex);
+          return prevTime - 1;
+        });
+      }, 1000);
+      return () => clearInterval(timerId);
+    } else if (
+      isExerciseFinished &&
+      currentExerciseIndex >= workoutDataExample.exercises.length
+    ) {
+      setRemainingTime(0);
+      setIsWorkoutFinished(true);
+      console.log("Workout finished");
+    }
+  }, [currentExerciseIndex, isExerciseFinished]);
+
+  console.log(currentExerciseIndex > workoutDataExample.exercises.length);
 
   // function to start the timer
   // function to pause the timer
@@ -108,36 +140,54 @@ useEffect(() => {
 
   return (
     <>
-      <section className="p-5 mx-auto body-font">
-        <div className="container mx-auto flex">
-          <div className="flex flex-col gap-2">
-            <div className="buttons flex h-8 w-8 gap-3">
-              <img src={PauseButton} alt="pause" />
-              <img src={StopButton} alt="stop" />
-            </div>
-            <div>
-              <p
-                className={
-                  workoutDataExample.exercises[currentExerciseIndex].name
-                }
-              ></p>
-            </div>
-            <div className="img">
-              <img
-                alt="exercise"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://dummyimage.com/400x400"
-              ></img>
+      {!isWorkoutFinished ? (
+        <section className="p-5 mx-auto body-font">
+          <div className="container mx-auto flex">
+            <div className="flex flex-col gap-2">
+              <div className="buttons flex h-8 w-8 gap-3">
+                <img src={PauseButton} alt="pause" />
+                <img src={StopButton} alt="stop" />
+              </div>
+              <div>
+                <p>
+                  {!isExerciseFinished
+                    ? workoutDataExample.exercises[currentExerciseIndex].name
+                    : "Rest time"}
+                </p>
+              </div>
+              <div className="img">
+                <img
+                  alt="exercise"
+                  className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                  /* src={workoutDataExample.exercises[currentExerciseIndex].image} */
+                  src={
+                    !isExerciseFinished
+                      ? workoutDataExample.exercises[currentExerciseIndex].image
+                      : "#"
+                  }
+                ></img>
 
-              <div>Remain time : {remainingTime}</div>
+                <div>
+                  Remain time :
+                  {!isExerciseFinished ? remainingTime : remainingTimeInRest}
+                </div>
+              </div>
+              <div>
+                Description :
+                {!isExerciseFinished
+                  ? workoutDataExample.exercises[currentExerciseIndex].description
+                  : "Relax"}
+              </div>
+              <div className="side panel">
+                <p>Next:</p>
+              </div>
             </div>
-            <div className="side panel">
-              <p>Next:</p>
-            </div>
+            <div className="bottom panel"></div>
           </div>
-          <div className="bottom panel"></div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <div>Workout Finished!</div>
+      )}
     </>
   );
 };

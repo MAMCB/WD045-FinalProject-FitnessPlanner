8a5fada1eb766880d155331489307bf6bae15e99
Darko Hristovski <hristovski15@gmail.com>
2024-02-01 17:39:33 +0100

    Workaut connected to the Workoud player

--- client/src/components/WorkoutPlan.jsx
+++ client/src/components/WorkoutPlan.jsx
@@ -73,6 +73,13 @@ const deleteHandlerExercises = (id) =>{
                       <p>Difficulty: {workout.difficulty}</p>
                       <p>Rating: {workout.rating}</p>
                       <div>
+                      <Link
+                        to={`/workoutPlayer/${workout._id}`}
+                          type="button"
+                          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        >
+                          Play
+                        </Link>
                         <Link
                         to={`/workoutPlan/${workout._id}`}
                           type="button"

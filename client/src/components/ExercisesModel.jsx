2fe05ade949432e1360a64a0385137520a346db3
AnnaShazk <dream1nd.find@gmail.com>
2024-02-12 18:03:55 +0100

    fix style in Template Store

--- client/src/components/ExercisesModel.jsx
+++ client/src/components/ExercisesModel.jsx
@@ -14,7 +14,7 @@ const ExercisesModel = ({ setIsModalOpn, workoutExer }) => {
         aria-hidden="true"
         className="h-lvh overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full backdrop-blur-md justify-center"
       >
-        <div className="relative p-4 w-full flex justify-center m-auto overflow-y-auto">
+        <div className="relative p-4 w-full flex justify-center m-auto ">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 md:w-1/2 w-lvw">
             <div className="flex justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 ">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
@@ -45,18 +45,21 @@ const ExercisesModel = ({ setIsModalOpn, workoutExer }) => {
               </button>
             </div>
             <div className="p-4 md:p-5 space-y-4">
-              <div id="accordion-collapse" data-accordion="collapse">
+              <div
+                id="accordion-collapse"
+                data-accordion="collapse"
+              >
                 {workoutExer.exercises.map((workout, index) => (
                   <div
                     key={index}
-                    className="border-b border-gray-200 dark:border-gray-700"
+                    className="border-b border-gray-200 dark:border-gray-700 "
                   >
                     <h2
                       id={`accordion-collapse-heading-${index}`}
                       onClick={() =>
                         setExpandedItem(expandedItem === index ? null : index)
                       }
-                      className="flex items-center justify-between p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                      className="flex items-center justify-between p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3 "
                       data-accordion-target={`#accordion-collapse-body-${index}`}
                       aria-expanded="true"
                       aria-controls={`accordion-collapse-body-${index}`}
@@ -67,7 +70,7 @@ const ExercisesModel = ({ setIsModalOpn, workoutExer }) => {
                       </span>
                       <svg
                         data-accordion-icon=""
-                        className="md:w-3 w-full h-3 rotate-180 shrink-0"
+                        className="md:w-3  h-3 rotate-180 shrink-0"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -87,7 +90,7 @@ const ExercisesModel = ({ setIsModalOpn, workoutExer }) => {
                       className={expandedItem === index ? "" : "hidden"}
                       aria-labelledby={`accordion-collapse-heading-${index}`}
                     >
-                      <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 flex flex-col lg:flex-row gap-3 overflow-hidden ">
+                      <div className="p-1 md:p-5 border border-b-0 border-gray-200 dark:border-gray-700 flex flex-col lg:flex-row gap-3">
                         <div>
                           <img
                             src={workout.exercise.image}
@@ -105,7 +108,6 @@ const ExercisesModel = ({ setIsModalOpn, workoutExer }) => {
                           <p className="text-gray-500 dark:text-gray-400">
                             Exercise Sets: {workout.sets}
                           </p>
-                          
                         </div>
                       </div>
                     </div>
